26f8f27eba0fbd2c2d8b0a5f66d1295c5a25cc60
Andrew Le <andrewle19@csu.fullerton.edu>
2017-09-23 13:08:21 -0700

    Shell V0.2 ADDED: Writes commands to History, List command to history TODO: Be able to run commands from history

--- shell.c
+++ shell.c
@@ -24,8 +24,61 @@ int quit(char *str)
   return 1;
 }
 
+// Count Line function
+// Parameters: file pointer
+// Function will count the number of lines in the Program
+// Returns the count of them
+int countLines(FILE *fp)
+{
+  fp = fopen("history.txt","r");
+  int count = 0;
+  char ch;
 
+  // if the file doesnt exist return count as 0
+  if (fp == NULL)
+  {
+    fclose(fp);
+    return count;
+  }
+  else
+  {
+    // loop till end of file
+    while(!feof(fp))
+    {
+      ch = fgetc(fp);
+      // checks if their is a newline if true increment count
+      if(ch == '\n')
+      {
+        count++;
+      }
+    }
+    fclose(fp);
+    return count;
+  }
+}
 
+void showHistory(FILE *fp, int lineCount)
+{
+  fp = fopen("history.txt","r");
+  char *line = malloc(MAX_LINE);
+
+  if(fp == NULL)
+  {
+    printf("No Commands in history\n");
+  }
+  else
+  {
+    for(int i = 0; i < lineCount; i++)
+    {
+      fgets(line,MAX_LINE,fp);
+      strtok(line,"\n");
+      printf("%i. %s\n",lineCount-i,line);
+    }
+
+  }
+  free(line);
+  fclose(fp);
+}
 
 int main(void)
 {
@@ -34,6 +87,10 @@ int main(void)
   char *arg = malloc(MAX_LINE);
   int run = 1; /* flag to determine when to exit program */
   int argc = 0; // count of arguments
+
+  int count = countLines(fp); // checks how many commands are in history
+
+
 
   while (run)
   {
@@ -46,7 +103,8 @@ int main(void)
     run = quit(arg); // check if user wants to quit or not
 
     args[0] = strtok(arg," "); // grab the first token
-    argc = 1; //increment the argument count
+    argc = 1; //set the argument count
+
     // parse/tokenize the commands into args array
     while (args[argc-1] != NULL)
     {
@@ -71,19 +129,29 @@ int main(void)
       }
       else
       {
+        // opens history file
         fp = fopen("history.txt","a");
-
+        // writes the arguments to the history file
         for(int i = 0; i < argc-1; i++)
         {
           fputs(args[i],fp);
           fputs(" ",fp);
         }
-        fputs("\n",fp);
+
+        fputs("\n",fp);// writes a newline in
+        count++; // increments count
         fclose(fp);
+
+        // NULL & 
         args[argc-2] = NULL;
         execvp(args[0],args);
 
       }
+    }
+    // if the user wants the history of commands
+    else if(strcmp(args[0],"history") == 0)
+    {
+      showHistory(fp,count);
     }
     // executes the command in child but parent wont wait so it will exit
     else
